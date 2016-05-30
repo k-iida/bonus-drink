4e50184b7729115d69d0ef5282db3b0e2ebbf470
飯田康介 <iida@under.gift>
2016-05-31 00:35:10 +0900

    解答

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,40 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    service = BonusDrinkService.new(amount)
+    service.execute!
+    service.empty_bottle_amount
+  end
+end
+
+
+class BonusDrinkService
+  EXCHANGE_BOTTLE_AMOUNT = 3
+
+  attr_reader :empty_bottle_amount
+
+  def initialize(drinkable_bottle_amount)
+    @drinkable_bottle_amount = drinkable_bottle_amount
+    @empty_bottle_amount = 0
+  end
+
+  def execute!
+    while @drinkable_bottle_amount > 0
+      drink
+    end
+  end
+
+  private
+
+  def drink
+    @drinkable_bottle_amount -= 1
+    @empty_bottle_amount += 1
+
+    exchange
+  end
+
+  def exchange
+    if @empty_bottle_amount % EXCHANGE_BOTTLE_AMOUNT == 0
+      @drinkable_bottle_amount += 1
+    end
   end
 end
